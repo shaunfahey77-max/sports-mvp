dad74836dcc2160824874a953a31340ec994dfb4
shaunfahey77-max <shaunfahey77@gmail.com>
2026-01-31 11:28:30 -0500

    Add Teams page and render at root route

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,24 +1,12 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Teams from "./pages/Teams";
-import TeamDetail from "./pages/TeamDetail";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
-        <h1>Sports MVP</h1>
-
-        <nav style={{ marginBottom: 16 }}>
-          <Link to="/" style={{ marginRight: 8 }}>Home</Link>
-          <Link to="/teams">Teams</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<div>Welcome 👋</div>} />
-          <Route path="/teams" element={<Teams />} />
-          <Route path="/teams/:id" element={<TeamDetail />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Teams />} />
+      </Routes>
     </BrowserRouter>
   );
 }
